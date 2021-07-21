e899a31ae2c0d06fd678197098bd06da4f01b94b
whhGithub <2698981813@qq.com>
2021-07-21 23:41:49 +0800

    update pwa

--- src/main.js
+++ src/main.js
@@ -7,5 +7,14 @@ import App from './App.vue'
 import 'vfonts/Lato.css'
 // 等宽字体
 import 'vfonts/FiraCode.css'
+import { registerSW } from 'virtual:pwa-register'
+const updateSW = registerSW({
+    onNeedRefresh() {
+        // show a prompt to user
+    },
+    onOfflineReady() {
+        // show a ready to work offline to user
+    },
+})
 
 createApp(App).use(router).use(store).mount('#app')
